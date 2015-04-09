d45091a8abb08cb052f9bd666fbfff690d427a3c
tripcase_user <nalinikanta.p@motivitylabs.com>
2015-04-09 18:14:04 +0530

    Nalini Kanta Initial Commit

---
+++ tripcase/parse-ml/src/main/java/com/motivity/labs/parse/file/reader/HtmlFileReader.java
@@ -0,0 +1,47 @@
+package com.motivity.labs.parse.file.reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ * 
+ * @author Nalini Kanta
+ * HtmlFileReader
+ */
+public class HtmlFileReader {
+	BufferedReader br = null;
+	StringBuilder sb = null;
+/**
+ * htmlFileRead
+ * @param filePath
+ * @return String
+ * @throws Throwable
+ */
+	public  String htmlFileRead(String filePath) throws Throwable{
+
+		try {
+
+			String sCurrentLine;
+			if(null!=filePath){
+			br = new BufferedReader(
+					new FileReader(filePath));
+			sb = new StringBuilder();
+			while ((sCurrentLine = br.readLine()) != null) {
+				sb.append(sCurrentLine);
+			}
+		}
+
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				throw ex;
+			}
+		}
+		return sb.toString();
+	}
+
+}
